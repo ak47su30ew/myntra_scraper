50eba1a7eb01bfd5ffb863eb1cc9320b15101910
ak47su30ew <ak47su30ew@gmail.com>
2022-10-25 22:34:37 +0530

    Add main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+rows = 50
+offset = 0
+
+base_url = f"https://www.myntra.com/gateway/v2/search/men-clothing?f=Categories%3AShirts&rows={rows}&o={offset}&plaEnabled=false&xdEnabled=false&pincode=560002"
